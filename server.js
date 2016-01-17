a118238dd45e240511c67267d02ed57a7678e1fe
elson lim <elsonlim7@gmail.com>
2016-01-18 02:11:35 +0800

    get todos

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 var app = require('express')();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+    id: 1,
+    description: 'Meet mom for lunch',
+    completed: false
+}, {
+    id: 2,
+    description: 'go supermarket',
+    completed: false
+}];
 
 app.get('/', function(req, res) {
     res.send('TODO api root');
+});
+
+app.get('/todos', function(req, res) {
+    res.send(todos);
+});
+
+app.get('/todos/:id', function(req, res) {
+    res.send(todos[id]);
 });
 
 app.listen(PORT, function () {
