b25aeda54ee6a0800cd9520c05105282c46d42ea
elson lim <elsonlim7@gmail.com>
2016-01-19 00:21:11 +0800

    add todos and todos/:is

--- server.js
+++ server.js
@@ -19,8 +19,21 @@ app.get('/todos', function(req, res) {
 });
 
 app.get('/todos/:id', function(req, res) {
-    res.send('Asking for todo with id of ' + req.params.id);
-    res.json(todos[req.params.id]);
+    var todoId = parseInt(req.params.id);
+    var todoObj;
+    
+    todos.forEach( function (item) {
+       if(item.id === todoId){
+           todoObj = item;
+       }     
+    });
+    
+    if(todoObj){
+        res.json(todoObj);
+    }else {
+        res.status(404).send();
+    }
+   // res.send('Asking for todo with id of ' + req.params.id);
 });
 
 app.listen(PORT, function () {
