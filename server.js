f0662067be2217bfea4e508d136c001c1639c084
elson lim <elsonlim7@gmail.com>
2016-01-20 00:27:54 +0800

    added pick to whitelist filter and trim body description

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var app = require('express')();
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var PORT = process.env.PORT || 3000;
 var todoNextId = 1;
 var todos = [];
@@ -16,13 +17,13 @@ app.get('/todos', function(req, res) {
 
 app.get('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id);
-    var todoObj;
+    var todoObj = _.findWhere(todos, {id: todoId});
     
-    todos.forEach( function (item) {
-       if(item.id === todoId){
-           todoObj = item;
-       }     
-    });
+//    todos.forEach( function (item) {
+//       if(item.id === todoId){
+//           todoObj = item;
+//       }     
+//    });
     
     if(todoObj){
         res.json(todoObj);
@@ -33,7 +34,11 @@ app.get('/todos/:id', function(req, res) {
 });
 
 app.post('/todos', function (req,res) {
-    var body = req.body;
+    var body = _.pick(req.body, 'description', 'completed');
+    body.description = body.description.trim();
+    if( !_.isBoolean(body.completed) || !_.isString(body.description) || body.description.length === 0){
+        return res.status(400).send();
+    }
     body.id = todoNextId++;
     todos.push(body);
     res.json(body);
