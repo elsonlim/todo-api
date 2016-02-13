5a09191e1872f09a1a72c462d5b5c96e03ba63dd
elson lim <elsonlim7@gmail.com>
2016-02-13 20:06:43 +0800

    add user.js

---
+++ models/user.js
@@ -0,0 +1,26 @@
+module.exports = function (sequelize, DataTypes) {
+	return sequelize.define('user', {
+		email: {
+			type: DataTypes.STRING,
+			unique: true,
+			validate: {
+				 isEmail: true
+			}
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [7,100],
+			}
+		}
+	}, {
+		hooks: {
+			beforeValidate: function (user, options) {
+				if( typeof user.email === 'string' ) {
+					user.email = user.email.toLowerCase();
+				}
+			}
+		}
+	});
+}
