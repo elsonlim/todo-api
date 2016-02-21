6def6e9dd610756080c13a25914e9e8afbccf809
elson lim <elsonlim7@gmail.com>
2016-02-22 00:28:55 +0800

    return token in header

--- server.js
+++ server.js
@@ -190,7 +190,7 @@ app.post('/users/login', function (req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 
 	db.user.authenticate(body).then(function (user) {
-		res.json(user.toPublicJson());
+		res.header('Auth', user.generateToken('authentication')).json(user.toPublicJson());
 	}, function () {
 		res.status(401).send();
 	});
